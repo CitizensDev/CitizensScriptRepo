037c320599c6ff8837f04ea6f72e3472b7cba200
AgentKid <agentkid20@gmail.com>
2012-08-24 15:20:42 -0400

    Added alphaID, modified slightly to be case insensitive

--- index.php
+++ index.php
@@ -5,6 +5,64 @@ session_start();
 $_SERVER['REQUEST_URI_PATH'] = preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']);
 $args = explode('/', trim($_SERVER['REQUEST_URI_PATH'], '/'));
 
+// 
+/**
+ * Translates a number to a short alhanumeric version
+ * 
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+ * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+ * @version   SVN: Release: $Id: alphaID.inc.php 344 2009-06-10 17:43:59Z kevin $
+ * @link      http://kevin.vanzonneveld.net/
+ * 
+ * @param mixed   $in     String or long input to translate     
+ * @param boolean $to_num Reverses translation when true
+ * @param mixed   $pad_up Number or boolean padds the result up to a specified length
+ * 
+ * @return mixed string or long
+ */
+function alphaID($in, $to_num = false, $pad_up = false)
+{
+    $index = "abcdefghijklmnopqrstuvwxyz0123456789";
+    $base  = strlen($index);
+ 
+    if ($to_num) {
+        // Digital number  <<--  alphabet letter code
+        $in  = strrev($in);
+        $out = 0;
+        $len = strlen($in) - 1;
+        for ($t = 0; $t <= $len; $t++) {
+            $bcpow = bcpow($base, $len - $t);
+            $out   = $out + strpos($index, substr($in, $t, 1)) * $bcpow;
+        }
+ 
+        if (is_numeric($pad_up)) {
+            $pad_up--;
+            if ($pad_up > 0) {
+                $out -= pow($base, $pad_up);
+            }
+        }
+    } else { 
+        // Digital number  -->>  alphabet letter code
+        if (is_numeric($pad_up)) {
+            $pad_up--;
+            if ($pad_up > 0) {
+                $in += pow($base, $pad_up);
+            }
+        }
+ 
+        $out = "";
+        for ($t = floor(log10($in) / log10($base)); $t >= 0; $t--) {
+            $a   = floor($in / bcpow($base, $t));
+            $out = $out . substr($index, $a, 1);
+            $in  = $in - ($a * bcpow($base, $t));
+        }
+        $out = strrev($out); // reverse
+    }
+ 
+    return $out;
+}
+
 // Smarty
 include('assets/Smarty/Smarty.class.php');
 $smarty = new Smarty;
@@ -54,13 +112,10 @@ switch(strtolower($args[0])){
     case 'user':
         break;
     case 'admin':
-        $output = 'assets/pages/admin.php';
         break;
     case '':
-        $output = 'assets/pages/index.php';
         break;
     default:
-        $output = 'assets/pages/404.php';
         break;
 }
 
@@ -77,5 +132,6 @@ if($page=='raw'){
     include('pages/raw.php');
     exit;
 }
+if(!isset($output)){ $smarty->assign('output', '404.tpl'); }else{ $smarty->assign('output', $output); }
 $smarty->display('index.tpl');
 ?>
