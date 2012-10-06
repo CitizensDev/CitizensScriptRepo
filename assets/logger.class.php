86742c4a9a159aacd9c41bf48106052cf58f76c2
AgentKid <agentkid20@gmail.com>
2012-10-06 13:00:26 -0400

    Added logger toggle to make the nyan happy.

--- assets/logger.class.php
+++ assets/logger.class.php
@@ -5,6 +5,8 @@ class Logger {
     private $accessLog="/var/log/scriptrepo/access.log";
     private $errorLog="/var/log/scriptrepo/error.log";
     public $username;
+    public $logErrors=true;
+    public $logAccess=false;
     public function __construct(){
         $this->errorHandle = fopen($this->errorLog, "a");
         $this->accessHandle = fopen($this->accessLog, "a");
@@ -14,12 +16,16 @@ class Logger {
         fclose($this->accessHandle);
     }
     public function accessLog($message){
-        if(isset($this->username)){ $append = "$this->username - "; }else{ $append=null; }
-        fwrite($this->accessHandle, $this->getPrefix().$append.$message."\n");
+        if($this->logAccess){
+            if(isset($this->username)){ $append = "$this->username - "; }else{ $append=null; }
+            fwrite($this->accessHandle, $this->getPrefix().$append.$message."\n");
+        }
     }
     public function errorLog($message){
-        if(isset($this->username)){ $append = "$this->username - "; }else{ $append=null; }
-        fwrite($this->errorHandle, $this->getPrefix().$append.$message."\n");
+        if($this->logErrors){
+            if(isset($this->username)){ $append = "$this->username - "; }else{ $append=null; }
+            fwrite($this->errorHandle, $this->getPrefix().$append.$message."\n");
+        }
     }
     private function getPrefix(){
         return "[".date("Y-m-d H:i:s")."] ".$_SERVER['REMOTE_ADDR']." - ";
